9d2315783bff5dc6d9240543f058a288cfb9c22f
Abhilasha Lahare <abhilashalahare6@gmail.com>
2026-01-03 11:39:34 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 // We allow both your Localhost (for testing) and your Vercel App (for production)
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://geo-fencing-attendance-system.vercel.app/" 
+  "https://geo-fencing-attendance-system.vercel.app" 
 ];
 
 app.use(cors({
